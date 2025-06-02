5dba30fb9ae5181a8b2abb711153ad076e0ae93b
Phelipe <ultimaterafa79@gmail.com>
2025-06-02 16:00:36 -0300

    :wrench: chore: alterando dependencias e configuracoes do bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { UsuarioModule } from './usuario/usuario.module';
 import { ProdutoModule } from './produto/produto.module';
 import { PostgresConfigService } from './config/postgres.config.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
-// Modulo principal da aplicação, importando o modulo de usuario
+// Modulo principal da aplicação, importando os modulos de usuario, produto e o nosso typeOrm
 @Module({
   imports: [
     UsuarioModule,
     ProdutoModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     TypeOrmModule.forRootAsync({
       useClass: PostgresConfigService,
       inject: [PostgresConfigService]
